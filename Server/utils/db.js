606c33874d934f026fe0002bdfaedc1c0a849dea
Garvit <taneja.garvit29@gmail.com>
2025-09-15 23:29:49 +0530

    updated utils

---
+++ Server/utils/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+        process.exit(1);
+    }
+};
+
+export default connectDB;

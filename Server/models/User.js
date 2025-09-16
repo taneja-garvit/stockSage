fd4de86c9b68a4c038c3ee1683dc5aba76df26e3
Garvit <taneja.garvit29@gmail.com>
2025-09-16 08:46:59 +0530

    update

--- Server/models/User.js
+++ Server/models/User.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password_hash: { type: String, required: true },
-    token_balance: { type: Number, default: 5000 },
+    token_balance: { type: Number, default: 500 },
     referral_code: { type: String, unique: true },
     referred_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null }
 });
